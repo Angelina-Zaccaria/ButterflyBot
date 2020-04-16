cd9ea929627511c6553de88ee4232a7bb6e11c0a
Brittany DiFede <bcd2998@gmail.com>
2020-04-16 18:32:09 -0400

    Users table created and commented out

--- data/config.js
+++ data/config.js
@@ -14,11 +14,22 @@ const pool = mysql.createPool(config);
 //Export the pool
 module.exports = pool;
 
-//Table Created
+//Table Created for Bad Words
 // config.connect(function(err) {
 //     if (err) throw err;
 //     console.log("Connected!");
 //     var sql = "CREATE TABLE BadWords (word VARCHAR(255))";
+//     config.query(sql, function(err, result) {
+//         if (err) throw err;
+//         console.log("Table created");
+//     });
+// });
+
+//Table created for Users
+// config.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "CREATE TABLE Users (id VARCHAR(255), name VARCHAR(255), starcount VARCHAR(255))";
 //     config.query(sql, function(err, result) {
 //         if (err) throw err;
 //         console.log("Table created");
