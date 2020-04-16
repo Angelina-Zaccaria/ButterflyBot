598c382d8087c94186395ba540076ea646339f28
Brittany DiFede <bcd2998@gmail.com>
2020-04-16 18:20:56 -0400

    bad word table created

--- data/config.js
+++ data/config.js
@@ -1,15 +1,25 @@
 const mysql = require('mysql');
 
 //Set database connection credentials
-const config = {
+const config = mysql.createConnection({
     host: '162.243.235.211',
     user: 'user345',
     password: 'Password1@',
     database: 'db345',
-};
+});
 
 //Create a MySQL pool
 const pool = mysql.createPool(config);
 
 //Export the pool
 module.exports = pool;
+
+config.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE BadWords (word VARCHAR(255))";
+    config.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
